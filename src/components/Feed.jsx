a1409420ea6236a166d29f21d1093157b87629b8
Dheeraj Raja <dheerajraja111@gmail.com>
2026-04-03 00:12:34 +0530

    User card added for connections

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      console.error("Error fetching feed:", error);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  if (!feed) return;
+
+  if (feed.length === 0) {
+    return <p className="text-center my-10">No more users to show.</p>;
+  }
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
---
+++ src/utils/requestSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const requestSlice = createSlice({
+  name: "request",
+  initialState: null,
+  reducers: {
+    addRequests: (state, action) => {
+      return action.payload;
+    },
+    removeRequest: (state, action) => {
+      const newAray = state.filter((request) => request._id !== action.payload);
+      return newAray;
+    },
+  },
+});
+
+export const { addRequests, removeRequest } = requestSlice.actions;
+
+export default requestSlice.reducer;
